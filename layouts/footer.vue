5fed1288138382bbd933eeaad050d664950a42e6
Fendy <zf@fendy5.cn>
2021-02-12 11:25:22 +0800

    feat: get-content

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="absolute w-full text-white text-center bottom-2">© 2021 <a href="https://fendy5.cn" class="primaryColor">Fendy</a> 版权所有 | <a class="primaryColor" href="https://github.com/fendy5/share">GitHub</a></p>
+  <p class="absolute w-full text-white text-center text-sm bottom-2">© 2021 <a href="https://fendy5.cn" class="primaryColor">Fendy</a> 版权所有 | <a class="primaryColor" href="https://github.com/fendy5/share">GitHub</a></p>
 </template>
 
 <script>
